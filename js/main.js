b81e7aad219a34a7de25b8d3cf081b86373e46eb
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-07-10 15:44:21 +0200

    Version 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 var app = {
   init: function() {
-    console.log('App initialisation');
+    console.log('App init');
+    app.onBurgerMenuClick();
   },
+
+  onBurgerMenuClick: function() {
+    const menu = document.querySelector('.menu-toggle');
+    menu.addEventListener('click', app.toggleClass)
+  },
+
+  toggleClass: function() {
+    const navigation = document.querySelector('.site-nav');
+    navigation.classList.toggle('site-nav--open');
+    // this = const menu
+    this.classList.toggle('open');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', app.init);
